f63484ee18efef3b9cef835874d5536cc1b6d59d
Damian Mee <mee.damian@gmail.com>
2016-02-05 12:00:10 +0800

    revert

--- run.js
+++ run.js
@@ -1,5 +1,2 @@
-#!/usr/bin/env node # --harmony --harmony_destructuring --harmony_default_parameters
-require("babel-register")({ignore: /(?!node_modules\/mee).*/});
-require("babel-polyfill");
-
+#!/usr/bin/env node --harmony --harmony_destructuring --harmony_default_parameters
 require('./src/');
