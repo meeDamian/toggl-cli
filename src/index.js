bad46ab7089e2e8585be0ca1f6a90529a7dae573
Alexandre Gagnon <alxgnon@gmail.com>
2016-03-31 17:17:43 -0400

    improve src/index.js shebang line compatibility
    fixes #4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-#!/usr/bin/env node --harmony_destructuring --harmony_default_parameters --harmony_rest_parameters
+#!/bin/sh
+':' //; exec "$(command -v nodejs || command -v node)" --harmony_destructuring --harmony_default_parameters --harmony_rest_parameters "$0" "$@"
 'use strict';
 
 require('./cli.js').main();
