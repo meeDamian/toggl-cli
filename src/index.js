ffeb1902b04adf1c12068c1f04f88dcbb2eb163f
Damian Mee <mee.damian@gmail.com>
2016-05-24 22:46:38 +0700

    node v6 compatibility fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 #!/bin/sh
-':' //; exec "$(command -v nodejs || command -v node)" --harmony_destructuring --harmony_default_parameters --harmony_rest_parameters "$0" "$@"
+':' //; NODE="$(command -v nodejs || command -v node)"; if [[ `$NODE --version` =~ ^v5\. ]]; then FLAGS="--harmony_destructuring --harmony_default_parameters --harmony_rest_parameters"; fi; exec "$NODE" $FLAGS "$0" "$@"
 'use strict';
 
 require('./cli.js').main();
