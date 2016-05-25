ac634d328e6a97ce00d5a2280b4a92c19491a6aa
Damian Mee <mee.damian@gmail.com>
2016-05-25 12:11:49 +0700

    sh compatibility added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 #!/bin/sh
-':' //; NODE="$(command -v nodejs || command -v node)"; if [[ `$NODE --version` =~ ^v5\. ]]; then FLAGS="--harmony_destructuring --harmony_default_parameters --harmony_rest_parameters"; fi; exec "$NODE" $FLAGS "$0" "$@"
+':' //; FLAGS=''; NODE="$(command -v nodejs || command -v node)"; if $(printf $($NODE --version) | grep -Eq '^v5\.'); then FLAGS="--harmony_destructuring --harmony_default_parameters --harmony_rest_parameters"; fi; exec "$NODE" $FLAGS "$0" "$@"
 'use strict';
 
 require('./cli.js').main();
